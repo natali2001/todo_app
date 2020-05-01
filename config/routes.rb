f16d319ad12435cdb1adae8ab1fda0d9cc3b6809
natali2001 <49942113+natali2001@users.noreply.github.com>
2020-05-02 00:18:10 +0300

    00

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :projects do
-   resources :tasks do
-    member do
-     patch :complete
-     end 
-   end 
-  end 
+  	resources :tasks do
+  		member do
+  			patch :complete
+  		end
+  	end
+  end
   get 'new'=>'projects#index'
-  get 'tasks'=>'tasks#index'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
