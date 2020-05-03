46ab8d510e678d5665e16d360923cf69bba1937b
natali2001 <49942113+natali2001@users.noreply.github.com>
2020-05-03 18:59:08 +0300

    add model User, page registration

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+validates :name, presence: true
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true
+          format: { with: VALID_EMAIL_REGEX }
+has_secure_password
+validates :password, length: { minimum: 6 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_220303) do
+ActiveRecord::Schema.define(version: 2020_05_03_153610) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2020_04_29_220303) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "completed_at"
     t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "tasks", "projects"
