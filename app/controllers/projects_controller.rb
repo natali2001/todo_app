2c3b7642b1945006beb849156cd3c9f3077e2d16
natali2001 <49942113+natali2001@users.noreply.github.com>
2020-09-15 04:26:18 +0300

    00

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -54,7 +54,7 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    @project.destroy
+    @projects.destroy
     respond_to do |format|
       format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
